2cb651cadadbf6df50f5f0f8c1fbff1a54b4990f
Igor Alex de Castro <igoralex8701@gmail.com>
2025-08-12 01:05:26 -0300

    feat: criado model e repostory do endereco e cliente

--- src/main/java/com/dio/projeto/ComProjetoDioApplication.java
+++ src/main/java/com/dio/projeto/ComProjetoDioApplication.java
@@ -1,4 +1,4 @@
-package com.dio.projeto.com_projeto_dio;
+package com.dio.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/dio/projeto/model/Cliente.java
@@ -0,0 +1,5 @@
+package com.dio.projeto.model;
+
+public class Cliente {
+
+}
