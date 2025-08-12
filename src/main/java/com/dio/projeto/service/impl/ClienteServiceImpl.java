e112a3f1e01b35c2c863b83a849f1d89392f0d14
Igor Alex de Castro <igoralex8701@gmail.com>
2025-08-12 01:34:55 -0300

    feat: add dependencia swagger, criado cliente controller e add @Service no ServiceClienteImpl

--- src/main/java/com/dio/projeto/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/dio/projeto/service/impl/ClienteServiceImpl.java
@@ -1,5 +1,41 @@
 package com.dio.projeto.service.impl;
 
-public class ClienteServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.dio.projeto.model.Cliente;
+import com.dio.projeto.service.ClienteService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
